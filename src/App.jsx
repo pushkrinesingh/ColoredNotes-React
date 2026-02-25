c3045c2c4808e070fd1a1bc52bdae97bab025063
Pushkrine Singh <your-email@example.com>
2026-02-25 16:55:43 +0530

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import ColoredNotes from "./ColoredNotes";
+
+const App = () => {
+  return <ColoredNotes />;
+};
+
+export default App;
---
+++ src/ColoredNotes.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { FaHandPointRight } from "react-icons/fa";
+import { FaHandPointDown } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+
+const ColoredNotes = () => {
+  const [Input, setInput] = useState("");
+  const [Color, setColor] = useState("#000000");
+  const [Notes, setNotes] = useState([]);
+
+  function AddNote() {
+    const obj = { id: Date.now(), input: Input, BGColor: Color };
+    setNotes([...Notes, obj]);
+    setInput("");
+  }
+  function DeleteNote(IdToDelete) {
+    setNotes(
+      Notes.filter((obj) => {
+        return obj.id !== IdToDelete;
+      }),
+    );
+  }
+  return (
+    <>
+      <div className="wrapper">
+        <h2>
+          Keep Your Notes Here <FaHandPointRight />
+        </h2>
+        <textarea
+          placeholder="Write A Note Here"
+          onChange={(e) => setInput(e.target.value)}
+          value={Input}
+        ></textarea>
+        <h3>
+          Select Your Color Of Choice <FaHandPointDown />
+        </h3>
+        <input
+          type="color"
+          onChange={(e) => setColor(e.target.value)}
+          value={Color}
+        />
+        <button onClick={AddNote}>Add Note</button>
+      </div>
+      <div className="container">
+        {Notes.map((obj) => {
+          return (
+            <div
+              key={obj.id}
+              style={{
+                backgroundColor: obj.BGColor,
+                width: "200px",
+                height: "200px",
+              }}
+            >
+              {obj.input}
+              <span>
+                <ImCross onClick={() => DeleteNote(obj.id)} />
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default ColoredNotes;
